e6127404a8ffbd76b47bb31ec574f5aa922fcfb2
Matthias Gerdin <matge373@student.liu.se>
2020-11-03 18:28:22 +0100

    ny lab4

--- lab4/robots/mainwindow.cpp
+++ lab4/robots/mainwindow.cpp
@@ -79,41 +79,45 @@ void MainWindow::keyPressEvent(QKeyEvent *e)
         bool actionTaken = false;
         bool waiting = false;
 
-        if (e->modifiers() == Qt::KeypadModifier) {
+        if ((e->modifiers() == Qt::NoModifier) || (e->modifiers() == Qt::KeypadModifier)) {
             switch (e->key()) {
+            case Qt::Key_Z:
             case Qt::Key_1:
                 actionTaken = tryMove(hero, Point{pt.x - 1, pt.y + 1});
                 break;
+            case Qt::Key_X:
             case Qt::Key_2:
                 actionTaken = tryMove(hero, Point{pt.x, pt.y + 1});
                 break;
+            case Qt::Key_C:
             case Qt::Key_3:
                 actionTaken = tryMove(hero, Point{pt.x + 1, pt.y + 1});
                 break;
+            case Qt::Key_A:
             case Qt::Key_4:
                 actionTaken = tryMove(hero, Point{pt.x - 1, pt.y});
                 break;
+            case Qt::Key_D:
             case Qt::Key_6:
                 actionTaken = tryMove(hero, Point{pt.x + 1, pt.y});
                 break;
+            case Qt::Key_Q:
             case Qt::Key_7:
                 actionTaken = tryMove(hero, Point{pt.x - 1, pt.y - 1});
                 break;
+            case Qt::Key_W:
             case Qt::Key_8:
                 actionTaken = tryMove(hero, Point{pt.x, pt.y - 1});
                 break;
+            case Qt::Key_E:
             case Qt::Key_9:
                 actionTaken = tryMove(hero, Point{pt.x + 1, pt.y - 1});
                 break;
+            case Qt::Key_S:
             case Qt::Key_5:
                 actionTaken = true;
                 waiting = true;
                 break;
-            default:
-                QWidget::keyPressEvent(e);
-            }
-        } else {
-            switch (e->key()) {
             case Qt::Key_T:
                 gameState.teleportHero();
                 actionTaken = true;
@@ -121,7 +125,7 @@ void MainWindow::keyPressEvent(QKeyEvent *e)
             default:
                 QWidget::keyPressEvent(e);
             }
-        }
+        } 
 
         if (actionTaken) { // process results of viable move
             processMove(waiting);
@@ -149,6 +153,10 @@ bool MainWindow::tryMove(Hero hero, const Point& point) {
  * Process results of viable move
  */
 void MainWindow::processMove(bool waiting) {
+  // GameState copy=gameState;
+  // copy.moveRobots();
+  // gameState=copy;
+
     gameState.moveRobots();
     score += gameState.countCollisions() * (POINTS_PER_ROBOT +
                                             (waiting ? WAIT_BONUS : 0));
