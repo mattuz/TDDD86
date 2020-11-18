585ae59818a5d200569d5c9d08f6b74af4c4f7b0
Matthias Gerdin <matge373@student.liu.se>
2020-11-18 20:18:50 +0100

    verkar vara något fel på destruktorn?

--- lab4/robots/mainwindow.cpp
+++ lab4/robots/mainwindow.cpp
@@ -155,13 +155,12 @@ bool MainWindow::tryMove(Hero hero, const Point& point) {
  * Process results of viable move
  */
 void MainWindow::processMove(bool waiting) {
-   std::cout << "processMove \n";
-   //GameState copy=gameState;
-   //copy.moveRobots();
-   //gameState=copy;
+   std::cout << "processMove "<<std::endl;
+   GameState copy=gameState;
+   copy.moveRobots();
+   gameState=copy;
 
-   gameState.moveRobots();
-   std::cout<<"RÖV";
+   //gameState.moveRobots();
     score += gameState.countCollisions() * (POINTS_PER_ROBOT +
                                             (waiting ? WAIT_BONUS : 0));
     gameState.draw(scene);
