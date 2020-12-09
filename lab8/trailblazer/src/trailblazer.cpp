aa4f9f5c3e12b13be788a8a1dba0043ecc24442d
Lisa Green <lisgr350@student.liu.se>
2020-12-09 18:54:10 +0100

    Någon slags DFS, oklart om det är rätt

--- lab8/trailblazer/src/trailblazer.cpp
+++ lab8/trailblazer/src/trailblazer.cpp
@@ -1,21 +1,46 @@
-// This is the CPP file you will edit and turn in.
-// Also remove these comments here and add your own, along with
-// comments on every function and on complex code sections.
+
 // TODO: write comment header for this file; remove this comment
 
 #include "costs.h"
 #include "trailblazer.h"
-// TODO: include any other headers you need; remove this comment
+
 using namespace std;
 
 vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
     vector<Vertex*> path;
+    Vertex* node;
+    start->setColor(GREEN);
+    start->visited = true;
+
+
+    if(start == end){
+        path.push_back(start);
+    }else{
+        for(auto arc : start->arcs){
+            if(!arc->visited){
+                arc->visited = true;
+                if(arc->finish != start){
+                    node = arc->finish;
+                }else{
+                    node = arc->start;
+                }
+                if(!node->visited){
+                    vector<Vertex*> tempPath = depthFirstSearch(graph, node, end);
+                    if(!tempPath.empty()){
+                        path = tempPath;
+                        path.insert(path.begin(), start);
+                        return path;
+                    }
+
+                }
+
+            }
+        }
+    }
+
     return path;
 }
+
 
 vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
     // TODO: implement this function; remove these comments
